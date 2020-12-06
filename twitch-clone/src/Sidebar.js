67cb2861796a253faf9fe54c636becd3f04a1103
mark-torres-10 <markptorres1@gmail.com>
2020-12-06 14:26:26 -0500

    finished sidebar

--- twitch-clone/src/Sidebar.js
+++ twitch-clone/src/Sidebar.js
@@ -5,14 +5,22 @@ import './Sidebar.css';
 const Sidebar = () => {
     return (
         <div className = "sidebar">
-            <h5>FOLLOWED CHANNELS</h5>
-            <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.o_LVqn47XZe1uEg9W0J91QHaDt&pid=Api" name="YouTube" followers = "43.02m"/>
-            <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.YKXXJCW1d2e7FDMGKbCJ6gHaFj&pid=Api" name="nigahiga" followers = "15.74m"/>
+            <div className="sidebar_top">
+                <h5>FOLLOWED CHANNELS</h5>
+                <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.o_LVqn47XZe1uEg9W0J91QHaDt&pid=Api" name="YouTube" followers = "43.02m"/>
+                <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.YKXXJCW1d2e7FDMGKbCJ6gHaFj&pid=Api" name="nigahiga" followers = "15.74m"/>
 
-            <h5>RECOMMENDED CHANNELS</h5>
-            <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.hzvheNqpDxigxKJYrWLZrAHaCw&pid=Api" name="ESPN" followers="1.23m"/>
+                <h5>RECOMMENDED CHANNELS</h5>
+                <Channel avatar="https://tse4.mm.bing.net/th?id=OIP.hzvheNqpDxigxKJYrWLZrAHaCw&pid=Api" name="ESPN" followers="1.23m"/>
 
-            <p className="sidebar_topShowMore">Show More</p>
+                <p className="sidebar_topShowMore">Show More</p>
+            </div>
+            <div className="sidebar_bottom">
+                <div className="sidebar_bottomContainer">
+                    <i className="fas fa-search"></i>
+                    <input type="text" placeholder="Search to Add Friends"/>
+                </div>
+            </div>
         </div>
     )
 }
