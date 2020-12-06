302786a31f15d0e9df438d9cfa56f06a7ff6b6b2
mark-torres-10 <markptorres1@gmail.com>
2020-12-06 13:26:26 -0500

    continue adding to header

--- twitch-clone/src/Header.js
+++ twitch-clone/src/Header.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+/*import { Avatar } from '@material-ui/core';*/
+import "./Header.css";
 
 const Header = () => {
     return (
@@ -10,12 +12,28 @@ const Header = () => {
                 <div className = "header_verticalLine"></div>
                 <h2>Esports</h2>
                 <h2>Music</h2>
+                <i className="fas fa-ellipsis-h"></i>
             </div>
+
+
             <div className="header_center">
-
+                <input type="text" placeholder="Search"/>
+                <div className="header_centerLogoContainer">
+                    <i className="fas fa-search"></i>
+                </div>
             </div>
-            <div className="header_right">
+            <div className="header_right"> 
+                <div className="header_rightContainer">
+                    <i className="fas fa-crown"></i>
+                    <i className="fas fa-inbox"></i>
+                    <i className="fas fa-comment-alt"></i>
 
+                    <div className="header_rightBits">
+                        <i className="fas fa-gem"></i>
+                        <h4>Get Bits</h4>
+                    </div>
+                    {/*<Avatar />*/}
+                </div>
             </div>
         </div>
     )
